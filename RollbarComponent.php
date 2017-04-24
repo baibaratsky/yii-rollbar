9df1e5db709396de2163a081e9865d9b6b35a516
Edoardo Tenani <edoardo.tenani@gmail.com>
2017-04-24 12:49:09 +0200

    preserve backward compatibility
    In order to not break the existing configurations, add
    a normalize function that converts old keys passed to the
    object to new ones.

--- RollbarComponent.php
+++ RollbarComponent.php
@@ -14,10 +14,13 @@ class RollbarComponent extends CApplicationComponent
         Rollbar::init($this->config, false, false);
         parent::init();
     }
-    
-    public function __set($key, $value) {
-      if ( $key === 'root' ) {
-        $value = YiiBase::getPathOfAlias( $value ) ? YiiBase::getPathOfAlias( $value ) : $value;
+
+    public function __set($key, $value)
+    {
+      $key = $this->normalizeWithCompatibility($key);
+      
+      if ($key === 'root') {
+          $value = Yii::getPathOfAlias( $value ) ? Yii::getPathOfAlias( $value ) : $value;
       }
       $this->config[$key] = $value;
     }
@@ -26,5 +29,30 @@ class RollbarComponent extends CApplicationComponent
     {
         $this->root = Yii::getPathOfAlias('application');
         $this->scrub_fields = ['passwd', 'password', 'secret', 'auth_token', 'YII_CSRF_TOKEN'];
+    }
+
+    /**
+     * Converts old public properties to new key names.
+     */
+    protected function normalizeWithCompatibility($key)
+    {
+        if ($key === 'rootAlias') {
+            $key = 'root';
+        }
+        $key = $this->underscore($key);
+    }
+
+    /**
+     * Converts any "CamelCased" into an "underscored_word".
+     *
+     * Taken from 
+     * https://github.com/yiisoft/yii2/blob/6da1ec6fb2b6367cb30d8c581575d09f0072812d/framework/helpers/BaseInflector.php#L411
+     * 
+     * @param string $words the word(s) to underscore
+     * @return string
+     */
+    private function underscore($words)
+    {
+        return strtolower(preg_replace('/(?<=\\w)([A-Z])/', '_\\1', $words));
     }
 }
